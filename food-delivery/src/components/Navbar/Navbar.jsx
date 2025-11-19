9d8a0e4821aa1589e319e85286e583e5541a4bb2
Rudraksh-200 <rudrakshcode58@gmail.comgit config --global user.email rudrakshcode58@gmail.com>
2025-11-19 20:42:31 +0530

    updated

---
+++ food-delivery/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Navbar.css'
+import {assets} from "../../assets/assets.js";
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className="logo"/>
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>About</li>
+        <li>Mobile-app</li>
+        <li>Contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="cart" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
