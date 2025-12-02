5c30ecc274ac510926d5e23e0d0dd40f1d4b0c90
Rudraksh-200 <rudrakshcode58@gmail.comgit config --global user.email rudrakshcode58@gmail.com>
2025-12-02 11:34:25 +0530

    initial commit

--- food-delivery/src/components/Footer/Footer.jsx
+++ food-delivery/src/components/Footer/Footer.jsx
@@ -10,17 +10,34 @@ const Footer = () => {
                 <img src={assets.logo}/>
                 <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, molestias?  
                 </p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon}/>
+                    <img src={assets.twitter_icon}/>
+                    <img src={assets.linkedin_icon}/>
+                </div>
+
             </div>
             <div className="footer-content-center">
-                <img src={assets.facebook_icon}/>
-                <img src={assets.twitter_icon}/>
-                <img src={assets.linkedin_icon}/>
+                <h2>Company</h2>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Delivery</li>
+                  <li>Privacy Policy</li>
+                </ul>
             </div>
             <div className="footer-content-right">
-                
+                <h2>Get in touch</h2>
+                <ul>
+                  <li>+91 8726389098</li>
+                  <li>contact@gamil.com</li>
+                </ul>
             </div>
-            
         </div>
+        <hr/>
+        <p className="footer-copyright">
+          Copyright 2024 @ Tomato.com - All rights reserved
+        </p>
       
     </div>
   )
--- food-delivery/src/components/Navbar/Navbar.jsx
+++ food-delivery/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css'
 import {assets} from "../../assets/assets.js";
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext.jsx';
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
   const [menu,setMenu]=useState("home");
+  const {getTotalCartAmount}=useContext(StoreContext);
+
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" className="logo"/>
+      <Link to='/'><img src={assets.logo} alt="" className="logo"/></Link>
       <ul className="navbar-menu">
-        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
-        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-app</li>
-        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact us</li>
+        <Link to='/'onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</ Link>
+        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
+        <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-app</a>
+        <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="cart" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+          <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
-        <button>sign in</button>
+        <button onClick={()=>setShowLogin(true)}>sign in</button>
 
       </div>
     </div>
